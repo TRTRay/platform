152b97bd672680e8ea34af9a5f053a091e111b04
TRT-master <1031080955@qq.com>
2024-01-18 22:00:37 +0800

    fix CORS policy

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,13 @@ from backend import create_app, log
 from flask_cors import CORS
 from gevent import pywsgi
 
+# 别放进mian()函数，影响跨域
+app = create_app()
+# 用来解决跨域问题
+CORS(app, resources=r"/*")
+
 
 def main():
-    app = create_app()
-    # 用来解决跨域问题
-    CORS(app, resources=r"/*")
 
     server = pywsgi.WSGIServer(
         ("0.0.0.0", 5000),
